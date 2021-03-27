032bd08cb781568b42a8c0a6ad78ee009fd50f6b
Chase Simmons <csim6890@gmail.com>
2021-03-26 21:22:48 -0500

    setup readclicks for menu options

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,9 @@
-const mM = document.querySelectorAll('.display');
+const mainMenuItem = document.querySelectorAll('.mainMenuItem');
 
-console.log(mM);
+function mainMenuItemClick() {
+  console.log(`i have been clicked -> ${this.classList[1]}`);
+}
+mainMenuItem.forEach((item) =>
+  item.addEventListener('click', mainMenuItemClick)
+);
+console.log(mainMenuItem);
